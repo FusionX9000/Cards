7e98b20c4060ac8cc60b31948f7d745c59f647f5
Salman Ahmad Khan <libertyshouts@gmail.com>
2019-10-19 09:38:08 +0530

    Fixed gallery images with  incorrect width when loading images

--- js/gallery.js
+++ js/gallery.js
@@ -5,10 +5,10 @@ input.addEventListener("change", (e)=>{
     let imageElement = document.querySelectorAll('.image img');
     
     if(files.length>imageElement.length) {
-    let cc = document.querySelector('.card-container');
-    for(i=0; i<files.length-imageElement.length; i++) {
-        cc.insertAdjacentHTML("beforeend",("<div class=\"card\"><div class=\"image\"><a href=\"#\"><img class=\"image\" src=\"#\"/></a></div></div>"))
-    }
+        let cc = document.querySelector('.card-container');
+        for(i=0; i<files.length-imageElement.length; i++) {
+            cc.insertAdjacentHTML("beforeend",('<div class="card"><div class="image"><a href="#"><img src="#"/></a></div></div>'))
+        }
     }
 
     imageElement = document.querySelectorAll('img');
@@ -17,8 +17,10 @@ input.addEventListener("change", (e)=>{
     let reader = new FileReader();
     reader.onload = ()=> {
         let dataURL = reader.result;
+        imageElement[i].onload = () => {
+            imageElement[i].parentNode.parentNode.parentNode.style.width = `${imageElement[i].width}px`;
+        }
         imageElement[i].src=dataURL;
-        imageElement[i].parentNode.parentNode.parentNode.style.width = `${imageElement[i].width}px`;
     };
     reader.readAsDataURL(files[i]);     }
 });  
